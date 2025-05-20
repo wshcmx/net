03b3526bfec1f3b735374c76420bf355b1dcfe14
umbrik <693254+umbrik@users.noreply.github.com>
2025-05-20 23:54:37 +0200

    Update

--- src/wshcmx/Templater.cs
+++ src/wshcmx/Templater.cs
@@ -6,13 +6,13 @@ namespace wshcmx;
 
 public static class Templater
 {
-    public static string Generate(string template, string data)
+    public static string Generate(string template, object[] data)
     {
         if (File.Exists(template))
         {
             template = File.ReadAllText(template);
         }
 
-        return Template.Compile(template).Render(JsonSerializer.Deserialize<Dictionary<string, object>>(data));
+        return Template.Compile(template).Render(data);
     }
 }
