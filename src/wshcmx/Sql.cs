983d108b7cf28bae60aa175ea9bc6f65bf7bf40a
umbrik <693254+umbrik@users.noreply.github.com>
2025-05-26 21:41:27 +0200

    Обновлен ExecutePaginationProcedure

--- src/wshcmx/Sql.cs
+++ src/wshcmx/Sql.cs
@@ -1,5 +1,6 @@
 using System.Data;
 using System.Text.Json;
+using System.Linq.Dynamic.Core;
 
 using Microsoft.Data.SqlClient;
 
@@ -49,16 +50,30 @@ public class Sql
         command.ExecuteNonQuery();
     }
 
-    public object[] ExecutePaginationProcedure(string procedureName, int page, int size, string serializedParameters)
+    public object[] ExecutePaginationProcedure(string procedureName, string serializedOptions, string serializedParameters)
     {
         ArgumentNullException.ThrowIfNull(_connectionString, nameof(_connectionString));
         ArgumentNullException.ThrowIfNull(serializedParameters, nameof(serializedParameters));
-        ArgumentNullException.ThrowIfNull(page, nameof(page));
-        ArgumentNullException.ThrowIfNull(size, nameof(size));
+
+        Dictionary<string, object>? options = JsonSerializer.Deserialize<Dictionary<string, object>>(serializedOptions);
+        _ = int.TryParse(options?.GetValueOrDefault("page")?.ToString(), out int page);
+
+        if (page < 1)
+        {
+            page = 1;
+        }
+
+        _ = int.TryParse(options?.GetValueOrDefault("size")?.ToString(), out int size);
+
+        if (size < 1 || size > 400)
+        {
+            size = 400;
+        }
+
+        string select = options?.GetValueOrDefault("select")?.ToString() ?? string.Empty;
+        string orderby = options?.GetValueOrDefault("orderby")?.ToString() ?? string.Empty;
 
         Dictionary<string, object>? parameters = JsonSerializer.Deserialize<Dictionary<string, object>>(serializedParameters);
-
-        ArgumentNullException.ThrowIfNull(parameters, nameof(parameters));
 
         using SqlConnection connection = new(_connectionString);
         using SqlCommand command = new(procedureName, connection)
@@ -66,23 +81,52 @@ public class Sql
             CommandType = CommandType.StoredProcedure
         };
 
-        foreach (var param in parameters)
+        if (parameters is not null)
         {
-            command.Parameters.AddWithValue(param.Key, param.Value is null ? DBNull.Value : param.Value.ToString());
+            foreach (var param in parameters)
+            {
+                command.Parameters.AddWithValue(param.Key, param.Value is null ? DBNull.Value : param.Value.ToString());
+            }
         }
 
         connection.Open();
         using SqlDataAdapter adapter = new(command);
         DataSet ds = new();
-        adapter.Fill(ds, page * size, size, ds.Tables[0].TableName);
+        adapter.Fill(ds);
+        var intermediateResult = ds.Tables[0].AsEnumerable().AsQueryable();
+
+        if (!string.IsNullOrEmpty(orderby))
+        {
+            intermediateResult = intermediateResult.OrderBy(orderby);
+        }
+
+        List<string> columns = new();
+
+        if (!string.IsNullOrEmpty(select))
+        {
+            select.Split(',')
+                .Select(s => s.Trim())
+                .ToList()
+                .ForEach(c => columns.Add(c));
+        }
+
+        bool hasSelect = columns.Count > 0;
+
+        intermediateResult = intermediateResult.Skip((page - 1) * size).Take(size);
+
         List<List<KeyValuePair<string, object?>>> rows = new(capacity: size);
 
-        foreach (DataRow row in ds.Tables[0].Rows)
+        foreach (DataRow row in intermediateResult)
         {
             List<KeyValuePair<string, object?>> rowList = new(ds.Tables[0].Columns.Count);
 
             foreach (DataColumn column in ds.Tables[0].Columns)
             {
+                if (hasSelect && !columns.Contains(column.ColumnName))
+                {
+                    continue;
+                }
+
                 rowList.Add(new(column.ColumnName, row[column] == DBNull.Value ? null : row[column]));
             }
 
