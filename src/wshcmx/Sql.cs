0c975571c0657c539391a1e349fe081cff14118e
SERROC <kido-exe@yandex.ru>
2026-03-09 18:46:04 +0300

    Добавлена поддержка postgres

--- src/wshcmx/Sql.cs
+++ src/wshcmx/Sql.cs
@@ -2,28 +2,28 @@ using System.Data;
 using System.Text.Json;
 using System.Linq.Dynamic.Core;
 
-using Microsoft.Data.SqlClient;
-
 namespace wshcmx;
 
 public class Sql
 {
     private string? _connectionString;
+    private IDatabaseProvider _provider = DatabaseProviderFactory.CreateProvider(DatabaseType.SqlServer); // Default to SQL Server
 
-    public void Init(string connectionString)
+    public void Init(string connectionString, DatabaseType databaseType = DatabaseType.SqlServer)
     {
         _connectionString = connectionString;
+        _provider = DatabaseProviderFactory.CreateProvider(databaseType);
     }
 
     public KeyValuePair<string, object?>[][] ExecuteQuery(string commandText)
     {
         GuardHelper.ThrowIfNull(_connectionString, nameof(_connectionString));
 
-        using SqlConnection connection = new(_connectionString);
-        using SqlCommand command = new(commandText, connection);
+        using var connection = _provider.CreateConnection(_connectionString);
+        using var command = _provider.CreateCommand(commandText, connection);
         connection.Open();
 
-        using SqlDataReader reader = command.ExecuteReader();
+        using var reader = command.ExecuteReader();
         List<List<KeyValuePair<string, object?>>> rows = new();
 
         while (reader.Read())
@@ -44,8 +44,9 @@ public class Sql
     public void ExecuteNonQuery(string commandText)
     {
         GuardHelper.ThrowIfNull(_connectionString, nameof(_connectionString));
-        using SqlConnection connection = new(_connectionString);
-        using SqlCommand command = new(commandText, connection);
+
+        using var connection = _provider.CreateConnection(_connectionString);
+        using var command = _provider.CreateCommand(commandText, connection);
         connection.Open();
         command.ExecuteNonQuery();
     }
@@ -75,22 +76,22 @@ public class Sql
 
         Dictionary<string, object>? parameters = JsonSerializer.Deserialize<Dictionary<string, object>>(serializedParameters);
 
-        using SqlConnection connection = new(_connectionString);
-        using SqlCommand command = new(procedureName, connection)
-        {
-            CommandType = CommandType.StoredProcedure
-        };
+        using var connection = _provider.CreateConnection(_connectionString);
+        using var command = _provider.CreateCommand(procedureName, connection);
+        command.CommandType = CommandType.StoredProcedure;
 
         if (parameters is not null)
         {
             foreach (var param in parameters)
             {
-                command.Parameters.AddWithValue(param.Key, param.Value is null ? DBNull.Value : param.Value.ToString());
+                var dbParam = _provider.CreateParameter(_provider.GetParameterPrefix() + param.Key,
+                    param.Value is null ? DBNull.Value : param.Value.ToString());
+                command.Parameters.Add(dbParam);
             }
         }
 
         connection.Open();
-        using SqlDataAdapter adapter = new(command);
+        using var adapter = _provider.CreateDataAdapter(command);
         DataSet ds = new();
         adapter.Fill(ds);
         var intermediateResult = ds.Tables[0].Rows.Cast<DataRow>().AsQueryable();
@@ -141,11 +142,9 @@ public class Sql
     {
         GuardHelper.ThrowIfNull(_connectionString, nameof(_connectionString));
 
-        using SqlConnection connection = new(_connectionString);
-        using SqlCommand command = new(procedureName, connection)
-        {
-            CommandType = CommandType.StoredProcedure
-        };
+        using var connection = _provider.CreateConnection(_connectionString);
+        using var command = _provider.CreateCommand(procedureName, connection);
+        command.CommandType = CommandType.StoredProcedure;
 
         if (serializedParameters is not null)
         {
@@ -155,13 +154,15 @@ public class Sql
             {
                 foreach (var param in parameters)
                 {
-                    command.Parameters.AddWithValue(param.Key, param.Value is null ? DBNull.Value : param.Value.ToString());
+                    var dbParam = _provider.CreateParameter(_provider.GetParameterPrefix() + param.Key,
+                        param.Value is null ? DBNull.Value : param.Value.ToString());
+                    command.Parameters.Add(dbParam);
                 }
             }
         }
 
         connection.Open();
-        using SqlDataAdapter adapter = new(command);
+        using var adapter = _provider.CreateDataAdapter(command);
         DataSet ds = new();
         adapter.Fill(ds);
         List<List<KeyValuePair<string, object?>>> rows = new();
